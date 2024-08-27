fce50b8b3434bfeef37736865360c1eafa4dd215
yofandirikiwinata <yofandirikiwinata24@gmail.com>
2024-08-27 22:20:49 +0700

    add: Manfaat Memisahkan Element Ke Masing-Masing Component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <button class="pagination-button" id="prevPage">&lt;</button>
+  <button class="pagination-button" id="nextPage">&gt;</button>
+</template>
+
+<style scoped>
+.pagination-button {
+  background-color: #fff;
+  color: #00b894;
+  padding: 8px 12px;
+  border: none;
+  border-radius: 50%;
+  cursor: pointer;
+  font-size: 16px;
+  margin: 0 5px;
+}
+</style>
