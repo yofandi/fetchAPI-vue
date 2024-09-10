7b48f3e75823f7aee44d03871ef01ddf3a1e8c53
yofandirikiwinata <yofandirikiwinata24@gmail.com>
2024-09-10 21:20:26 +0700

    add: Cara Membuat Restful API Tanpa Backend

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,6 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-  </header>
   <RouterView />
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,56 +1,11 @@
 <script setup>
 import ProductCard from "@/components/ProductCard.vue";
 import Pagination from "@/components/Pagination.vue";
-import {
-  onBeforeMount,
-  onBeforeUnmount,
-  onBeforeUpdate,
-  onMounted,
-  onUpdated,
-  ref,
-} from "vue";
-
-const page = ref(1);
-
-function nextPage() {
-  page.value++;
-}
-
-onBeforeMount(() => {
-  console.log("hai ini onBeforeMounted");
-  // lakukan persiapan atau sefek samping sebelum mounting
-});
-
-onMounted(() => {
-  console.log("hai ini onMounted");
-  //lakukan tugas setelah mounting, seperti fetching data
-});
-
-onBeforeUpdate(() => {
-  console.log("Component akan segera tampil");
-  // lakukan persiapa atau efek samping sebelum pembaruan
-});
-
-onUpdated(() => {
-  console.log("Component sudah diperbaharui");
-  // lakukan tugas setelah pembaruan, jika diperlukan
-});
-
-onBeforeUnmount(() => {
-  console.log("component will be unmounted soon");
-  // lalukan pembersihan atau persiapan sebelum unmounting
-});
-
-onMounted(() => {
-  console.log("component has been unmounted");
-  // lakukan tugas terakhir sebelum dihapus
-});
+import { ref } from "vue";
 </script>
 
 <template>
   <main>
-    {{ page }}
-    <button @click="nextPage">Next</button>
     <div class="product-grid">
       <ProductCard></ProductCard>
     </div>
