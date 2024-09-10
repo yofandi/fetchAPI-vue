2070292be1b82e0336321fb4ef02fae8ae4a54df
yofandirikiwinata <yofandirikiwinata24@gmail.com>
2024-09-10 21:34:28 +0700

    add: Cara Fetch Data Dengan Axios Dan Async Component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,28 @@
 <script setup>
 import ProductCard from "@/components/ProductCard.vue";
 import Pagination from "@/components/Pagination.vue";
+
 import { ref } from "vue";
+import axios from "axios";
+
+const products = ref([]);
+
+products.value = await axios
+  .get("http://localhost:3000/products")
+  .then((res) => res.data);
+console.log(products.value);
+// async function getProducts() {
+//   const response = await axios.get("http://localhost:3000/products");
+//   products.value = response.data;
+//   console.log(response.data);
+// }
+
+// getProducts();
 </script>
 
 <template>
   <main>
+    {{ products }}
     <div class="product-grid">
       <ProductCard></ProductCard>
     </div>
